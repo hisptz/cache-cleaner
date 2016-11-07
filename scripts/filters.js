0a8f55a6fd506eafecf850c60f1fd010901dac99
Joseph Chingalo <profschingalo@gmail.com>
2016-11-07 15:41:28 +0300

    update key values

--- scripts/filters.js
+++ scripts/filters.js
@@ -2,4 +2,15 @@
 
 /* Filters */
 
-var cacheCleanerFilters = angular.module('cacheCleanerFilters', []);
+var cacheCleanerFilters = angular.module('cacheCleanerFilters', [])
+    .filter('renameCacheOrLocalStorage', function() {
+        return function(input) {
+            input = input.replace(".", "-");
+            input = input.replace("dhis2","ARDS Web Portal");
+            return input;
+        };
+    }).filter('renameIdDb', function() {
+        return function(input) {
+            return input.replace("dhis2","ARDS Web Portal ");
+        };
+    });
